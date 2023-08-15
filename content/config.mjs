9eac02b73edbf68bedbc687ce3e0544c3675bd8c
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2023-08-15 08:23:31 +0100

    Update author URL

--- content/config.mjs
+++ content/config.mjs
@@ -3,6 +3,6 @@ export default {
 	description: "The digital garden of Daniel Metcalfe (aka Homostellaris)",
 	author: "Daniel Tariq Metcalfe",
 	authorLogo: "/assets/Profile photo - colour.jpg",
-	domain: "https://flowershow-sigma.vercel.app/",
+	domain: "https://homostellaris.io/",
 	navLinks: [{ href: "/about", name: "About" }],
 };
