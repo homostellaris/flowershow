9e8d736fb42012011e5254ce1f81f67c5f3a44a0
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2024-01-31 20:18:30 +0000

    Remove author logo
    Not sure what it does ATM.

--- content/config.mjs
+++ content/config.mjs
@@ -2,7 +2,6 @@ export default {
 	title: "Homostellaris notes",
 	description: "The digital garden of Daniel Metcalfe (aka Homostellaris)",
 	author: "Daniel Tariq Metcalfe",
-	authorLogo: "/images/profile-photo-colour.jpg",
 	domain: "https://homostellaris.io/",
 	showSidebar: true,
 	analyticsConfig: {
